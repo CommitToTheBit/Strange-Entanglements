6ecec2acad6808833b07507d70162a2fa5a22bc2
CommitToTheBit <samueldrysdale@googlemail.com>
2026-02-08 13:03:14 +0000

    Start adding operations.

--- extensions/strange-entanglements-cpp/StrangeQuantumState.cpp
+++ extensions/strange-entanglements-cpp/StrangeQuantumState.cpp
@@ -3,10 +3,72 @@
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/classes/engine.hpp>
 
-// ----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 // StrangeQuantumState::_bind_methods:
-// ----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 void StrangeQuantumState::_bind_methods()
 {
 	
+}
+
+// -----------------------------------------------------------------------------
+// StrangeQuantumState::_ready:
+// -----------------------------------------------------------------------------
+void StrangeQuantumState::_ready()
+{
+	Node::_ready();
+
+	// -------------------------------------------------------------------------
+	// DEBUG:
+	// -------------------------------------------------------------------------
+	DoHadamard(0);
+	DoHadamard(1);
+
+	for (Vector2 state : mSuperposition)
+	{
+		UtilityFunctions::print(state);
+	}
+}
+
+// -----------------------------------------------------------------------------
+// StrangeQuantumState::DoSingleQubitOperation: Apply a single qubit operation
+// to a given qubit.
+// -----------------------------------------------------------------------------
+void StrangeQuantumState::DoSingleQubitOperation(array<array<float, 2>, 2> const& operation, int qubit)
+{
+	vector<Vector2> new_superposition = vector<Vector2>(mSuperposition.size());
+
+	int bit = 1 << qubit;
+	for (int state = 0; state < mSuperposition.size(); ++state)
+	{
+		int row = state & bit;
+		for (int i = 0; i < 2; ++i)
+		{
+			new_superposition[state ^ row + i * bit] += operation[row][i] * mSuperposition[state];
+		}
+	}
+
+	mSuperposition = new_superposition;
+}
+
+// -----------------------------------------------------------------------------
+// StrangeQuantumState::DoHadamard: Apply a Hadamard operation to a given qubit.
+// -----------------------------------------------------------------------------
+void StrangeQuantumState::DoHadamard(int qubit)
+{
+	DoSingleQubitOperation({ { (float)cos(Math_PI / 4),  (float)sin(Math_PI / 4) }, { (float)sin(Math_PI / 4),  -(float)cos(Math_PI / 4) } }, qubit);
+
+	for (int state = 0; state < mSuperposition.size(); ++state)
+	{
+		mSuperposition[state] = (10000.0 * mSuperposition[state]).round() / 10000.0;
+	}
+}
+
+// -----------------------------------------------------------------------------
+// StrangeQuantumState::DoControlledNot: Apply a controlled not operation to a
+// given qubit.
+// -----------------------------------------------------------------------------
+void DoControlledNot(int qubit, int control_qubit, int control_state)
+{
+
 }
--- extensions/strange-entanglements-cpp/StrangeQuantumState.h
+++ extensions/strange-entanglements-cpp/StrangeQuantumState.h
@@ -2,9 +2,12 @@
 #define STRANGE_QUANTUM_STATE_H
 
 #include <godot_cpp/classes/node.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+
+#include <vector>
 
 using namespace std;
-using namespace godot; 
+using namespace godot;
 
 // ----------------------------------------------------------------------------
 // StrangeQuantumState: Node to manage quantum state of a puzzle.
@@ -12,6 +15,34 @@ using namespace godot;
 class StrangeQuantumState : public Node
 {
 	GDCLASS(StrangeQuantumState, Node)
+
+public:
+	// ------------------------------------------------------------------------
+	// State
+	// ------------------------------------------------------------------------
+	int mQubits = 2;
+	vector<Vector2> mSuperposition =
+	{
+		Vector2(1.0, 0.0),
+		Vector2(0.0, 0.0),
+		Vector2(0.0, 0.0),
+		Vector2(0.0, 0.0),
+	};
+
+public:
+	// ------------------------------------------------------------------------
+	// Godot
+	// ------------------------------------------------------------------------
+	virtual void _ready() override;
+
+	// ------------------------------------------------------------------------
+	// Operations
+	// ------------------------------------------------------------------------
+	void DoSingleQubitOperation(array<array<float, 2>, 2> const& operation, int qubit);
+
+
+	void DoHadamard(int qubit);
+	void DoControlledNot(int qubit, int control_qubit, int control_state);
 
 protected:
 	// ------------------------------------------------------------------------
